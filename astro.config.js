ea88600f527d05a3b006b04aebaac7d3849196d7
jinzhoule <2508024603@qq.com>
2024-12-19 23:05:38 +0800

    feat: test

--- astro.config.js
+++ astro.config.js
@@ -19,7 +19,7 @@ import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://drowsyindra.github.io',
+  site: 'https://jinzhoull.github.io/',
   integrations: [tailwind(), react(), sitemap()],
   markdown: {
     syntaxHighlight: false,
